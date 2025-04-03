c3269399d79e3af3acee44c0ae7f7609f4e559b9
Gorish Gupta <gorishgupta50@gmail.com>
2025-04-03 23:29:59 +0530

    Update DS2874.java

--- DS2874.java
+++ DS2874.java
@@ -1,25 +1,22 @@
 public class DS2874 {
-    public class Solution {
-        public long maximumTripletValue(int[] nums) {
-            int n = nums.length;
-            int[] leftMax = new int[n];
-            int[] rightMax = new int[n];
-            for (int i = 1; i < n; i++) {
-                leftMax[i] = Math.max(leftMax[i - 1], nums[i - 1]);
-                rightMax[n - 1 - i] = Math.max(rightMax[n - i], nums[n - i]);
-            }
-            long res = 0;
-            for (int j = 1; j < n - 1; j++) {
-                res = Math.max(res, (long) (leftMax[j] - nums[j]) * rightMax[j]);
-            }
-            return res;
+    public long maximumTripletValue(int[] nums) {
+        int n = nums.length;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], nums[i - 1]);
+            rightMax[n - 1 - i] = Math.max(rightMax[n - i], nums[n - i]);
         }
+        long res = 0;
+        for (int j = 1; j < n - 1; j++) {
+            res = Math.max(res, (long) (leftMax[j] - nums[j]) * rightMax[j]);
+        }
+        return res;
     }
+}
 
-    public static void main(String[] args) {
-        DS2874 ds2874 = new DS2874();
-        Solution solution = ds2874.new Solution();
-        int[] nums = { 1, 2, 3, 4, 5 };
-        System.out.println(solution.maximumTripletValue(nums)); // Output: 6
-    }
+public static void main(String[] args) {
+    DS2874 ds2874 = new DS2874();
+    int[] nums = { 1, 2, 3, 4, 5 };
+    System.out.println(ds2874.maximumTripletValue(nums)); // Output: 6
 }
