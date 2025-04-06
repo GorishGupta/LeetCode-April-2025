54c507143b1e3632b427512e092935b2ba0c61a4
Gorish Gupta <gorishgupta50@gmail.com>
2025-04-06 23:43:17 +0530

    Update DS2879.java

--- DS2879.java
+++ DS2879.java
@@ -1,21 +1,24 @@
 public class DS2879 {
-    public long maximumTripletValue(int[] nums) {
-        long maxTriplet = 0;
-        for (int i = 0; i < nums.length; i++) {
-            for (int k = nums.length - 1; k > i; k--) {
-                int j = i + 1;
-                while (j < k) {
-                    maxTriplet = Math.max(maxTriplet, (long) (nums[i] - nums[j]) * nums[k]);
-                    j++;
+    public class Solution {
+        public long maximumTripletValue(int[] nums) {
+            long maxTriplet = 0;
+            for (int i = 0; i < nums.length; i++) {
+                for (int k = nums.length - 1; k > i; k--) {
+                    int j = i + 1;
+                    while (j < k) {
+                        maxTriplet = Math.max(maxTriplet, (long) (nums[i] - nums[j]) * nums[k]);
+                        j++;
+                    }
                 }
             }
+            return Math.max(0, maxTriplet);
         }
-        return Math.max(0, maxTriplet);
     }
-}
 
-public static void main(String[] args) {
-    DS2879 solution = new DS2879();
-    int[] nums = { 1, 2, 3, 4, 5 };
-    System.out.println(solution.maximumTripletValue(nums)); // Output: 12
+    public static void main(String[] args) {
+        DS2879 ds2879 = new DS2879();
+        Solution solution = ds2879.new Solution();
+        int[] nums = { 1, 2, 3, 4, 5 };
+        System.out.println(solution.maximumTripletValue(nums)); // Output: 6
+    }
 }
