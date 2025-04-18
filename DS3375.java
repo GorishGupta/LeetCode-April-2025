e42b3c30fba2d68824df468f6ca3db831fdceee0
Gorish Gupta <gorishgupta50@gmail.com>
2025-04-18 22:33:29 +0530

    Update DS3375.java

--- DS3375.java
+++ DS3375.java
@@ -1,27 +1,25 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
-class Solution {
-    public int minOperations(int[] nums, int k) {
-        Arrays.sort(nums);
-        int mini = nums[0];
-        if (mini < k)
-            return -1;
+public static int minOperations(int[] nums, int k) {
+    Arrays.sort(nums);
+    int mini = nums[0];
+    if (mini < k)
+        return -1;
 
-        int cnt = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > k) {
-                cnt++;
-                while (i + 1 < nums.length && nums[i] == nums[i + 1])
-                    i++;
-            }
+    int cnt = 0;
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] > k) {
+            cnt++;
+            while (i + 1 < nums.length && nums[i] == nums[i + 1])
+                i++;
         }
-        return cnt;
     }
+    return cnt;
+}
 
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        int[] nums = { 1, 2, 3, 4, 5 };
-        int k = 3;
-        System.out.println(sol.minOperations(nums, k)); // Output: 2
-    }
+public static void main(String[] args) {
+    int[] nums = { 1, 2, 3, 4, 5 };
+    int k = 3;
+    System.out.println(minOperations(nums, k)); // Output: 2
 }
