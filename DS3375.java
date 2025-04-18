86fb3ba389d86230b094eb141dc22a78843109b1
Gorish Gupta <gorishgupta50@gmail.com>
2025-04-18 22:34:40 +0530

    Update DS3375.java

--- DS3375.java
+++ DS3375.java
@@ -1,25 +1,28 @@
 import java.util.Arrays;
-import java.util.Scanner;
 
-public static int minOperations(int[] nums, int k) {
-    Arrays.sort(nums);
-    int mini = nums[0];
-    if (mini < k)
-        return -1;
+class DS3375 {
+    public static int minOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int mini = nums[0];
+        if (mini < k)
+            return -1;
 
-    int cnt = 0;
-    for (int i = 0; i < nums.length; i++) {
-        if (nums[i] > k) {
-            cnt++;
-            while (i + 1 < nums.length && nums[i] == nums[i + 1])
-                i++;
+        int cnt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > k) {
+                cnt++;
+                while (i + 1 < nums.length && nums[i] == nums[i + 1])
+                    i++;
+            }
         }
+        return cnt;
     }
-    return cnt;
 }
 
 public static void main(String[] args) {
+    DS3375 ds = new DS3375();
+
     int[] nums = { 1, 2, 3, 4, 5 };
     int k = 3;
-    System.out.println(minOperations(nums, k)); // Output: 2
+    System.out.println(ds.minOperations(nums, k)); // Output: 2
 }
