e05e46fe1a252321d0e85a75ded560213d1bd12c
Gorish Gupta <gorishgupta50@gmail.com>
2025-04-18 22:31:54 +0530

    Update DS38.java

--- DS38.java
+++ DS38.java
@@ -1,34 +1,32 @@
 public class DS38 {
-    class Solution {
-        public String countAndSay(int n) {
-            String result = "1";
-            for (int i = 2; i <= n; i++) {
-                result = generateNextTerm(result);
-            }
-            return result;
+    public String countAndSay(int n) {
+        String result = "1";
+        for (int i = 2; i <= n; i++) {
+            result = generateNextTerm(result);
         }
+        return result;
+    }
 
-        private String generateNextTerm(String currentTerm) {
-            StringBuilder nextTerm = new StringBuilder();
-            int count = 1;
-            for (int i = 1; i < currentTerm.length(); i++) {
-                if (currentTerm.charAt(i) == currentTerm.charAt(i - 1)) {
-                    count++;
-                } else {
-                    nextTerm.append(count).append(currentTerm.charAt(i - 1));
+    private String generateNextTerm(String currentTerm) {
+        StringBuilder nextTerm = new StringBuilder();
+        int count = 1;
+        for (int i = 1; i < currentTerm.length(); i++) {
+            if (currentTerm.charAt(i) == currentTerm.charAt(i - 1)) {
+                count++;
+            } else {
+                nextTerm.append(count).append(currentTerm.charAt(i - 1));
 
-                    count = 1;
-                }
+                count = 1;
             }
-            nextTerm.append(count).append(currentTerm.charAt(currentTerm.length() - 1));
-            return nextTerm.toString();
         }
+        nextTerm.append(count).append(currentTerm.charAt(currentTerm.length() - 1));
+        return nextTerm.toString();
+
     }
 
     public static void main(String[] args) {
         DS38 ds = new DS38();
-        Solution sol = ds.new Solution();
         int n = 5; // Example input
-        System.out.println(sol.countAndSay(n)); // Output: "111221"
+        System.out.println(ds.countAndSay(n)); // Output: "111221"
     }
 }
