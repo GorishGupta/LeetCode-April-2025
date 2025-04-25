7bb4f7cfcf9ce169c8f4b18b7ef81a41635ca8d3
Gorish Gupta <gorishgupta50@gmail.com>
2025-04-25 10:39:18 +0530

    Update DS2845.java

--- DS2845.java
+++ DS2845.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class DS2845 {
     class Solution {
         public long countInterestingSubarrays(List<Integer> nums, int modulo, int k) {
