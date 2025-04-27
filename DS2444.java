e739d21a2818d47919467283842162a01aa2754e
Gorish Gupta <gorishgupta50@gmail.com>
2025-04-27 23:23:30 +0530

    Update DS2444.java

--- DS2444.java
+++ DS2444.java
@@ -1,17 +1,23 @@
 public class DS2444 {
-    class Solution {
-        public long countSubarrays(int[] nums, int minK, int maxK) {
-            int left = -1, min = -1, max = -1, n = nums.length;
-            long count = 0l;
-            for (int i = 0; i < n; i++) 
-                if (nums[i] < minK || nums[i] > maxK) 
-                    left = min = max = i;
-                else {
-                    min = nums[i] == minK ? i : min;
-                    max = nums[i] == maxK ? i : max;
-                    count += (min < max ? min : max) - left;
-                } 
-            return count;
-        }
+    public long countSubarrays(int[] nums, int minK, int maxK) {
+        int left = -1, min = -1, max = -1, n = nums.length;
+        long count = 0l;
+        for (int i = 0; i < n; i++)
+            if (nums[i] < minK || nums[i] > maxK)
+                left = min = max = i;
+            else {
+                min = nums[i] == minK ? i : min;
+                max = nums[i] == maxK ? i : max;
+                count += (min < max ? min : max) - left;
+            }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        DS2444 newDs2444 = new DS2444();
+        int[] nums = { 1, 3, 5, 2, 7, 5 };
+        int minK = 1;
+        int maxK = 5;
+        System.out.println(newDs2444.countSubarrays(nums, minK, maxK)); // Output: 14
     }
 }
